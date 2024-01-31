6b70aad32f855289e584af0efbadfbafe2bfff5d
igor-kostovski <igor.kostovski98@gmail.com>
2024-01-31 21:35:54 +0100

    adding basic react/vite apps

---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,14 @@
+import Youtube from './Youtube'
+
+function App() {
+
+  return (
+    <>
+      <h1>created Vite app</h1>
+      <h1>created Vite app</h1>
+      <Youtube />
+    </>
+  )
+}
+
+export default App
